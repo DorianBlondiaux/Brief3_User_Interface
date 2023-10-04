ea7be14e759d8461a5311568297703e7ea88aab1
Blondiaux Dorian <dorian5959@gmail.com>
2023-10-04 14:12:20 +0200

    fix fermeture

--- scripts.js
+++ scripts.js
@@ -3,6 +3,14 @@ var isPcSize = window.matchMedia("(min-width: 800px)").matches;
 var navBarSize = 33;
 var lastId = "";
 var ratio = 1.2;
+const lorem = `Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+ sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+   aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+     sunt in culpa qui officia deserunt mollit anim id est laborum.`;
+
+createHeaderFooter();
 
 if (isPcSize){ 
   document.getElementById("navBtn").style.visibility = "hidden"; 
@@ -113,14 +121,13 @@ function getUserListHtml(){
   promiseRes.then((result) =>
     result.data.map((user)=>
     document.getElementById("content").innerHTML += ` 
-                  <div class="card-body" onclick="openModal(${user.id})">
-                    <img  src="${
+                  <div class="card-body"">
+                    <img onclick="openModal(${user.id})" src="${
                       user.avatar
                     }" class="img_item" alt="...">
                       <div class="valise_contenu_card">
                         <div class="contenu_card">
-                          <h5>${user.first_name}</h5>
-                          <p class="descr_article">${user.last_name}</p>
+                          <h6>${user.first_name} ${user.last_name}</h6>
                         </div>
                       </div>
                       
@@ -132,10 +139,9 @@ function getUserListHtml(){
                           }" class="img_item" alt="...">
                             <div class="valise_contenu_card">
                               <div class="contenu_card">
-                                <h5>${user.first_name}</h5>
-                                <p class="descr_article">${user.last_name}</p>
-                                <p class="descr_article">${user.email}</p>
-                                <button id="${user.id}" onclick="checkUser(this.id)" class="btn btn-primary">Ajouter</button>
+                                <h6>${user.first_name} ${user.last_name}</h6>
+                                <p href="mailto:${user.email}" class="descr_article">Mail: ${user.email}</p>
+                                <p class="descr_article">${lorem}</p>
                               </div>
                             </div>
                         </div>
@@ -146,12 +152,62 @@ function getUserListHtml(){
   }
 
 
-function getUserCreate(){
+async function getUserCreate(){
+  const response = await fetch("https://reqres.in/api/users", {
+  method: 'POST',
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  },
+  body: `{
+    "name": "dorian",
+    "job": "dev"
+}`,
+  });
 
+  response.json().then(data => {
+    console.log(JSON.stringify(data));
+  });
+
+  
+}
+
+async function getUser(id){
+  const response = await fetch('https://reqres.in/api/users/' + id);
+  const usersJson= await response.json();
 }
 
 function createHeaderFooter(){
-  
+  document.getElementById('root').innerHTML =
+  `<div id="leftNavbar" class="sidebar">
+  <div class="navBarChoix">
+      <p class="toHover" id="userList" onclick="loadContent(this.id); if(!isPcSize){openCloseNav();}">Liste des utilisateurs</p>
+      <div class="navbar-list-divider"></div>
+      <p class="toHover" id="userCreate" onclick="loadContent(this.id); if(!isPcSize){openCloseNav();}">Créer utilisateur</p>
+      <div class="navbar-list-divider"></div>
+  </div>
+</div>
+
+<div id="main">
+  <div id="topBar">
+      <div class="entete">
+          <span id="nom">UserTech Solutions</span>
+      </div>
+      <button class="openbtn" id="navBtn" onclick="openCloseNav()">☰</button>
+  </div>
+
+  <div id="content">
+  </div>
+
+  <footer>
+      <h5>Pages</h5>
+      <div>
+          <p class="toHover" id="userList2" onclick="loadContent(this.id)">Liste des utilisateurs</p>
+          <p class="toHover" id="userCreate2" onclick="loadContent(this.id)">Créer utilisateur</p>
+      </div>
+  </footer>
+
+</div>`;
 }
 
 // When the user clicks on the button, open the modal
@@ -161,7 +217,5 @@ function openModal(id) {
 
 // When the user clicks on <span> (x), close the modal
 function closeModal(id) {
-  console.log("myModal" + id);
-  console.log(document.getElementById("myModal" + id));
   document.getElementById("myModal" + id).style.display = "none";
 }
